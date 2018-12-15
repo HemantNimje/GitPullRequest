769a26774a6eaccd46b3947994d108548856bc23
Hemantkumar Nimje <nimje.hemant@gmail.com>
2018-12-14 22:31:26 -0800

    Initial commit

---
+++ app/src/main/java/com/example/hemant/gitpullrequest/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.hemant.gitpullrequest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
